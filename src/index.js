862836d28c0c4ee7d5be9416c1881019742d2c31
Bruno Sanches <bysanches@gmail.com>
2017-10-25 19:25:09 -0200

    First styling

--- src/index.js
+++ src/index.js
@@ -15,16 +15,29 @@ const getImageUrl = url => {
     return `images/${imageName}`;
 };
 
+const renderPaymentConditions = conditions => {
+    let rgx = /(\dx de R\$ \d+,\d{2})/;
+    return conditions.replace(rgx, part => `<span class="payment-conditions">${part}</span>`)
+};
+
 const renderProduct = item => {
     return (
-        `<div class="product">
-            <img src="${getImageUrl(item.imageName)}" alt="">
-            <p>${item.name}</p>
-            <div>Por: <span class="price">${item.price}</span></div> 
-            <div>
-                <span class="paymentConditions">
-                    ${item.productInfo.paymentConditions}
-                </span>
+        `<div class="product card col-md">
+            <div class="card-body">
+                <img src="${getImageUrl(item.imageName)}">
+                <p class="card-text">${item.name}</p>
+                <div>Por: <span class="price">${item.price}</span></div> 
+                <div>
+                    <span class="paymentConditions">
+                        ${renderPaymentConditions(item.productInfo.paymentConditions)}
+                    </span>
+                </div>
+            </div>
+            <div class="actions">
+                <button type="button" class="btn">
+                    adicionar ao carrinho
+                    <i class="material-icons">add_shopping_cart</i>
+                </button>
             </div>
         </div>`
     );
@@ -37,4 +50,18 @@ fetchProducts(data => {
     let recommendation = data[0].data.recommendation;
     let recommendationHtml = recommendation.slice(0, 3).reduce((html, item) => html += renderProduct(item), '');
     document.getElementById('recommendation').innerHTML = recommendationHtml;
+    
+    function addToggleActions(card) {
+        card.addEventListener('mouseover', () => {
+            card.querySelector('.actions').style.display = 'block';
+        });
+        card.addEventListener('mouseout', () => {
+            card.querySelector('.actions').style.display = 'none';
+        });
+    }
+
+    let cards = document.querySelectorAll('.card');
+    for (let card of cards) {
+        addToggleActions(card);
+    }
 })
